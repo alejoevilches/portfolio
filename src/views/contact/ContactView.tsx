088479b5c92838f5372916ef5ea8a15a572fb5cf
Alejo Vilches <alejoevilches@gmail.com>
2024-10-12 02:35:19 -0300

    start contact page

---
+++ src/views/contact/ContactView.tsx
@@ -0,0 +1,18 @@
+import LinkedInIcon from "@/components/generics/LinkedinIcon/LinkedinIcon";
+import MailIcon from "@/components/generics/MailIcon/MailIcon";
+import DiscordIcon from "@/components/generics/DiscordLogo/DiscordIcon";
+import { TitleComponent } from "@/components/generics/TitleComponent/TitleComponent";
+
+export default function ContactView(){
+    return (
+        <>
+            <TitleComponent title="Contact me" />
+            <section className="flex flex-row gap-4 justify-center">
+                <LinkedInIcon style={{ fill: '#9ca3af' }}/>
+                <MailIcon style={{ fill: '#9ca3af' }}/>
+                <DiscordIcon style={{ fill: '#9ca3af' }}/>
+            </section>
+        </>
+    )
+}
+
