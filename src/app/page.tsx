2ca73d8875493a85decf3fe70b7ad222df4fd09a
Alejo Vilches <alejoevilches@gmail.com>
2024-12-29 21:53:27 -0300

    add hover effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Projects from "@/components/Projects/Projects";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-center py-20 px-4 text-5xl">A software developer inspired by the fear of being average</h1>
+      <h1 className="text-center py-20 px-4 text-6xl">A software developer inspired by the fear of being average</h1>
       <Projects />
     </main>
   )
--- src/components/Projects/ProjectsCard.tsx
+++ src/components/Projects/ProjectsCard.tsx
@@ -8,16 +8,19 @@ interface IProjectsCardProps{
 export default function ProjectsCard({project}:IProjectsCardProps){
     const {title, description, tags, image}=project
     return (
-        <article className="relative text-white flex flex-col justify-end 
-        h-80 mx-4 bg-gray-500 bg-cover rounded-2xl pb-2"
-        style={{ backgroundImage: `url(${image})` }}>
-            <h2 className="text-white text-2xl pl-2 font-semibold">{title}</h2>
-            <p className="px-2">{description}</p>
-            <div data-testid="tags" className="flex flex-row flex-wrap pl-2">
+        <article 
+            className="relative text-white flex flex-col justify-end h-80 mx-4 bg-gray-500 bg-cover rounded-2xl pb-2 group"
+            style={{ backgroundImage: `url(${image})` }}
+        >
+            <div className="hidden group-hover:block">
+                <h2 className="text-white text-2xl pl-2 font-semibold">{title}</h2>
+                <p className="px-2">{description}</p>
+                <div data-testid="tags" className="flex flex-row flex-wrap pl-2">
                     {tags.map((tag, index) => (
                         <SkillChip key={index} skill={tag} />
                     ))}
                 </div>
+            </div>
         </article>
     )
 }
